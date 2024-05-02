6f5981785067118e0d5d80c9aaa60fe66862530c
JosefKnize <pepatork@gmail.com>
2024-05-02 20:14:33 +0200

    Removed landingManager constant connection values

--- arcor2_AREditor/Assets/HOLOLENS/Scripts/Managers/HLandingManager.cs
+++ arcor2_AREditor/Assets/HOLOLENS/Scripts/Managers/HLandingManager.cs
@@ -23,10 +23,11 @@ public class HLandingManager : Singleton<HLandingManager>
         Debug.Assert(port != null);
         Debug.Assert(user != null);
         GameManagerH.Instance.OnConnectedToServer += ConnectedToServer;
-        // GameManagerH.Instance.OnDisconnectedFromServer += DisconnectedFromServer;
-        // domain.text = PlayerPrefs.GetString("arserver_domain", "");
+
+        domain.text = PlayerPrefs.GetString("arserver_domain", "");
         port.text = PlayerPrefs.GetInt("arserver_port", 6789).ToString();
         user.text = PlayerPrefs.GetString("arserver_username", "user1").ToString();
+
         connectButton.OnClicked.AddListener(() => ConnectToServer(true));
     }
 
@@ -41,19 +42,10 @@ public class HLandingManager : Singleton<HLandingManager>
         }
 
         var trimedDomain = domain.text.Trim();
-
-        trimedDomain = "192.168.104.100";
-
-#if UNITY_EDITOR
-        trimedDomain = "127.0.0.1";
-#endif
-
-
         int portInt = int.Parse(port.text);
         PlayerPrefs.SetString("arserver_domain", trimedDomain);
         PlayerPrefs.SetInt("arserver_port", portInt);
         PlayerPrefs.SetString("arserver_username", user.text);
-        // PlayerPrefs.SetInt("arserver_keep_connected", KeepConnected.isOn ? 1 : 0);
         PlayerPrefs.Save();
         GameManagerH.Instance.ConnectToSever(trimedDomain, portInt);
     }
@@ -66,10 +58,5 @@ public class HLandingManager : Singleton<HLandingManager>
     private void ConnectedToServer(object sender, EventArgs args)
     {
         landingScreen.SetActive(false);
-    }
-
-    private void DisconnectedFromServer(object sender, EventArgs args)
-    {
-        landingScreen.SetActive(true);
     }
 }
